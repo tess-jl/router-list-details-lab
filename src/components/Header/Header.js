2e7e11d1836debf0cc88d984fa4644da31fb6f68
Tess Lameyer <tjlameyer@gmail.com>
2020-02-05 13:49:25 -0800

    Header has a link back to list homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'; 
+import { Link } from 'react-router-dom';
+
 
 const Header = () => (
   <header>
     <h1>Rick and Morty... again?! yes, again</h1>
+
+    <nav>
+      <Link to='/'>back to list</Link>
+    </nav>
   </header>
 );
 
